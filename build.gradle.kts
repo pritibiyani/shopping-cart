bfdc0235a18332a099a24f496982d66c6ab8291c
Priti <priti@Pritis-MacBook-Pro.local>
2024-05-31 15:00:41 +0800

    Carissa/Priti: Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.3.0"
+	id("io.spring.dependency-management") version "1.1.5"
+}
+
+group = "com.shoppingcart"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	runtimeOnly("com.h2database:h2")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
